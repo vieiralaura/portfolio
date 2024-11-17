55ddbc714f17cfb3c43d4fff79b28aad2d993b88
Laura Ferreira de Bello Vieira <laura.vieira@clear.sale>
2024-11-16 21:43:34 -0300

    day03

--- 100 days of code/day02/tip_calculator.py
+++ 100 days of code/day02/tip_calculator.py
@@ -1,6 +1,12 @@
+#welcome message for the user
 print("Welcome to the tip calculator!")
+#ask for the total bill input as save it in a variable "total"
 total = float(input("What was the total bill?$"))
+#ask for the percent tip input as save it in a variable "percent"
 percent = int(input("How much tip would you like to give?10, 12 or 15?"))
+#ask for the number of people to split the bill input as save it in a variable "people"
 people = int(input("How many people to split the bill?"))
+#calculate the tip
 tip = (total + total*percent/100)/people
+#print the tip for each people
 print(f"Each person should pay: ${tip}")
